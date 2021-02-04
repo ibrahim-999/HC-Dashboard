120cb771b0897ebb2c92e07aa67af472f2c7dd39
ibrahim-khalaf <ibrahimkhalaf99@gmail.com>
2021-02-04 04:43:16 +0200

    initial commit

---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
+import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatDividerModule} from '@angular/material/divider';
+import { MatCardModule} from '@angular/material/card';
+import { DashboardService } from 'src/app/modules/dashboard.service';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+
+
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    PostsComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    FlexLayoutModule,
+    MatDividerModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatTableModule
+  ],
+  providers: [
+    DashboardService
+  ]
+})
+export class DefaultModule { }  
---
+++ src/app/shared/widgets/area/area.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import  * as Highcharts from 'highcharts';
+
+@Component({
+  selector: 'app-widget-area',
+  templateUrl: './area.component.html',
+  styleUrls: ['./area.component.scss']
+})
+export class AreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
